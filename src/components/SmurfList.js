9126c81b1438c8a1f67e1e1169676fdc77fe28bc
Alex Hoskins <alexander.hoskins@yahoo.com>
2021-11-05 09:54:54 -0700

    reducer setup and mapstatetoprops set up on smurflist

--- src/components/SmurfList.js
+++ src/components/SmurfList.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import Smurf from './Smurf';
+import { connect } from 'react-redux';
 
- const SmurfList = ()=> {
-    const isLoading = false;
+ const SmurfList = (props)=> {
+    console.log('props from smurflist', props)
+    console.log('here are your', props.smurfs)
     const testSmurf = {
         id:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9",
         name:'Poppa Smurf',
@@ -11,16 +13,28 @@ import Smurf from './Smurf';
         description: 'Papa is the practical village leader and the father figure of 100 or so young Smurfs. He is easily identified by his red Smurf hat, pants, and a shortly-trimmed white beard and moustache.'
     }
 
-    if (isLoading) {
+    if (props.isFetching) {
         return <h1>Loading...</h1>;
     }
 
     return(<div className="listContainer">
-        <Smurf smurf={testSmurf}/>
+        {
+        props.smurfs.map(smrf=>{
+            return <Smurf smurf={smrf}/>
+        })
+        }
     </div>);
 }
+const mapStateToProps = state => {
+    console.log('props from ms2p', state)
+    return {
+      smurfs: state.smurfs,
+      isFetching: state.isFetching,
+      error: state.error
+    };
+  };
 
-export default SmurfList;
+export default connect(mapStateToProps)(SmurfList);
 
 //Task List:
 //1. Connect the smurfs and loading state values to the SmurfList component.
--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import thunk from 'redux-thunk';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
 import "./index.css";
 import App from "./App";
-
+import reducer from './reducers'
 const { worker } = require('./mocks/browser');
 worker.start();
 
 const rootElement = document.getElementById("root");
+const store = createStore(reducer, applyMiddleware(thunk));
 
 ReactDOM.render(
-    <App />, 
+    <Provider store={store}>
+        <App />
+    </Provider>, 
     rootElement
 );
 
